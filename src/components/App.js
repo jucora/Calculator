cb48da5194961098fdc6bfb29c04e8887811b900
jucora <artjulius@hotmail.com>
2020-07-21 08:10:20 -0500

    Import calculate to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
-import React from 'react';
-import Display from './Display';
-import ButtonPanel from './ButtonPanel';
-import '../css/style.scss';
+import React from "react";
+import Display from "./Display";
+import ButtonPanel from "./ButtonPanel";
+import "../css/style.scss";
+/* import calculate from '../logic/calculate';-> COMMENTED FOR NOW 
+                                                  TO AVOID no-unused-vars 
+                                                 ERROR. IT WILL BE IMPLEMMENTED IN NEXT MILESTONE */
 
 const App = () => (
   <div className="app">
