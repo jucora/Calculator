3a2a93b03f871ddf8824440f577cb9f7691219f1
jucora <artjulius@hotmail.com>
2020-07-22 10:15:30 -0500

    Add divide by zero validation

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,19 +1,24 @@
-const Big = require('big.js');
+import Big from "big.js";
 
 const operate = (numberOne, numberTwo, operation) => {
   const value1 = new Big(numberOne);
   const value2 = new Big(numberTwo);
 
   switch (operation) {
-    case '+':
+    case "+":
       return value1.plus(value2);
-    case '-':
+    case "-":
       return value1.minus(value2);
-    case 'X':
+    case "x":
       return value1.times(value2);
-    case '÷':
-      return value1.div(value2);
-    case '%':
+    case "÷":
+      console.log("div", numberTwo);
+      if (numberTwo !== "0") {
+        return value1.div(value2);
+      } else {
+        return "Invalid operation!";
+      }
+    case "%":
       return value1.times(value2).div(100);
     default:
       break;
